a7c21784425be0e1213a9cbb3da740edcb5f890e
gccunanan <gccunanan@gmail.com>
2019-12-01 13:38:42 -0800

    updated VideoItem alt atribute to title instead of description

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -8,7 +8,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
         // onClick needs an arrow function to be able to pass a video into onVideoSelect
         // back up to the parent component (App) using the callback method inside of the class App component
         <div onClick={() => onVideoSelect(video)} className="video-item item">
-            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} />
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
             </div>
